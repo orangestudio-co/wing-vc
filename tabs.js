010eab9aa0519baea7a240ff9ce21c1360cc942a
Noah Shreve <122912407+nshreve@users.noreply.github.com>
2026-07-10 22:29:25 -0400

    Add timer to tabs

--- tabs.js
+++ tabs.js
@@ -6,6 +6,9 @@
     const tab = Array.from(c.querySelectorAll("[data-tabs='item']"));
     const visual = Array.from(c.querySelectorAll("[data-tabs='visual'] > *"));
 
+    let currentIndex = 0;
+    let autoplayTimer = null;
+
     const activate = (index) => {
       const targetVisual = visual[index]?.children.length
         ? visual[index]
@@ -14,13 +17,31 @@
       visual.forEach((el) => el.classList.remove("is-active"));
       tab[index].classList.add("is-active");
       targetVisual.classList.add("is-active");
+      currentIndex = index;
+    };
+
+    const stopAutoplay = () => {
+      if (autoplayTimer) {
+        clearInterval(autoplayTimer);
+        autoplayTimer = null;
+      }
+    };
+
+    const startAutoplay = () => {
+      autoplayTimer = setInterval(() => {
+        activate((currentIndex + 1) % tab.length);
+      }, 5000);
     };
 
     tab.forEach((t, index) => {
-      t.addEventListener("click", () => activate(index));
+      t.addEventListener("click", () => {
+        stopAutoplay();
+        activate(index);
+      });
     });
 
     // Init
     activate(0);
+    if (tab.length > 1) startAutoplay();
   });
 }
